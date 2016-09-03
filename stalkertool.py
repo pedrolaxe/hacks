612d05c46ac72d740e88d79627bdaf9ea45c0a32
pedrolaxe <pedrolaxe@gmail.com>
2016-09-03 01:53:16 -0300

    add youtube

--- stalkertool.py
+++ stalkertool.py
@@ -4,13 +4,14 @@ import requests
 
 print "/******"
 print "*"
-print "* StalkerTool v1.0.2"
+print "* StalkerTool v1.0.4"
 print "* Author: Pedro Laxe"
 print "*"
 print "******/"
 print "1 - Facebook"
 print "2 - Twitter"
 print "3 - Instagram"
+print "4 - Youtube"
 print "0 - Todas as Redes"
 print "\n"
 
@@ -52,6 +53,16 @@ def searchinsta(email):
     else:
         print "Instagram não encontrado!"
 
+def searchyt(email):
+    n = email.find('@')
+    username = email[0:n]
+
+    url = "https://youtube.com/%s" %username
+    r = requests.get(url)
+    if r.status_code == 200:
+        print "Perfil no Youtube em Potêncial -> %s" %url
+    else:
+        print "Youtube não encontrado!"
 
 if option == 1:
     searchfb(email)
@@ -59,7 +70,10 @@ elif option == 2:
     searchtw(email)
 elif option == 3:
     searchinsta(email)
+elif option == 4:
+    searchyt(email)
 elif option == 0:
     searchfb(email)
     searchtw(email)
     searchinsta(email)
+    searchyt(email)
