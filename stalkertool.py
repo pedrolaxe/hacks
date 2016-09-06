bfc48aed0aef211c3a86a51863ca0572a521e544
pedrolaxe <pedrolaxe@gmail.com>
2016-09-06 12:20:53 -0300

    Added Google Plus

--- stalkertool.py
+++ stalkertool.py
@@ -3,7 +3,7 @@ import requests
 
 print "/******"
 print "*"
-print "* StalkerTool v1.0.4"
+print "* StalkerTool v1.0.5"
 print "* Author: Pedro Laxe"
 print "*"
 print "******/"
@@ -11,6 +11,7 @@ print "1 - Facebook"
 print "2 - Twitter"
 print "3 - Instagram"
 print "4 - Youtube"
+print "5 - Google +"
 print "0 - All Social Netwoks"
 print "\n"
 
@@ -28,7 +29,7 @@ def searchfb(email):
         print "Potential Facebook profile -> %s" %url
     else:
         busca = "https://www.facebook.com/search/top/?q=%s" %email
-        print "Facebook not found, try the search: %s" %busca
+        print "Try the Facebook search: %s" %busca
 
 def searchtw(email):
     n = email.find('@')
@@ -63,6 +64,17 @@ def searchyt(email):
     else:
         print "Youtube not found!"
 
+def searchgp(email):
+    n = email.find('@')
+    username = email[0:n]
+
+    url = "https://plus.google.com/s/%s/top" %username
+    r = requests.get(url)
+    if r.status_code == 200:
+        print "Try the Google Plus search -> %s" %url
+    else:
+        print "Google Plus not found!"
+
 if option == 1:
     searchfb(email)
 elif option == 2:
@@ -71,8 +83,11 @@ elif option == 3:
     searchinsta(email)
 elif option == 4:
     searchyt(email)
+elif option == 5:
+    searchgp(email)
 elif option == 0:
     searchfb(email)
     searchtw(email)
     searchinsta(email)
     searchyt(email)
+    searchgp(email)
